b34a10ce3f3132619ccf60913a5f26ba096dd185
coder-anil01 <145540918+coder-anil01@users.noreply.github.com>
2024-03-16 17:20:19 +0530

    finish

--- client/src/page/Pricing.jsx
+++ client/src/page/Pricing.jsx
@@ -5,7 +5,8 @@ import { Link } from 'react-router-dom';
 
 const Pricing = () => {
 
-
+    let plan1Pay = "https://payments.cashfree.com/links/u6dvkbvbc040"
+    let plan2Pay = "https://payments.cashfree.com/links/K6dvkkrvm040"
   return (
     <>
     <div className="pricing">
@@ -13,22 +14,24 @@ const Pricing = () => {
         <div className="prcing-container">
             <div className="pricing-card">
                 <div className="pricing-card-heading">One Time</div>
-                <div className="pricing-price"><span>₹499</span>₹149</div>
-                <div className="pricing-des"><PiCaretDoubleRightBold/> calibration calibration</div>
-                <div className="pricing-des"><PiCaretDoubleRightBold/> calibration Paid Pormation</div>
-                <div className="pricing-des"><PiCaretDoubleRightBold/> calibration Paid Pormation</div>
-                <Link to='https://payments.cashfree.com/links/J6dkqog1pk40'>
+                <div className="pricing-price"><span>₹499</span>₹99</div>
+                <div className="pricing-des"><PiCaretDoubleRightBold/> Video Call Sex</div>
+                <div className="pricing-des"><PiCaretDoubleRightBold/> All Body Part View</div>
+                <div className="pricing-des"><PiCaretDoubleRightBold/> Safe Video Call</div>
+                <div className="pricing-des"><PiCaretDoubleRightBold/> Camera On/Off Fitures Available</div>
+                <Link to={plan1Pay}>
                     <div className="pricing-button">Book Now</div>
                 </Link>
             </div>
 
             <div className="pricing-card">
                 <div className="pricing-card-heading">One Month</div>
-                <div className="pricing-price"><span>₹4999</span>₹1499</div>
-                <div className="pricing-des"><PiCaretDoubleRightBold/> calibration calibration</div>
-                <div className="pricing-des"><PiCaretDoubleRightBold/> calibration Paid Pormation</div>
-                <div className="pricing-des"><PiCaretDoubleRightBold/> calibration Paid Pormation</div>
-                <Link to='https://payments-test.cashfree.com/links/b6dl5g6q4kpg'>
+                <div className="pricing-price"><span>₹4999</span>₹999</div>
+                <div className="pricing-des"><PiCaretDoubleRightBold/> Video Call Sex</div>
+                <div className="pricing-des"><PiCaretDoubleRightBold/> All Body Part View</div>
+                <div className="pricing-des"><PiCaretDoubleRightBold/> Safe Video Call</div>
+                <div className="pricing-des"><PiCaretDoubleRightBold/> Camera On/Off Fitures Available</div>
+                <Link to={plan2Pay}>
                     <div className="pricing-button">Book Now</div>
                 </Link>
             </div>
